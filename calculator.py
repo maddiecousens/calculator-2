b4176785be57713971df8bb6e9e39d3345989ab1
Hackbright Student <no-reply@hackbrightacademy.com>
2016-09-28 17:18:00 -0700

    added map function to tokens

--- calculator.py
+++ calculator.py
@@ -13,9 +13,26 @@ run = True
 while run:
     user_input = raw_input('> ')
     tokens = user_input.split(' ')
+    args = map(int, tokens[1:])
     if tokens[0] == "q":
         run = False
     elif tokens[0] == "+":
-        print add(int(tokens[1]), int(tokens[2]))
+        print add(args[0], args[1])
     elif tokens[0] == "-":
-        print subtract(int(tokens[1]), int(tokens[2]))
+        print subtract(args[0], args[1])
+    elif tokens[0] == "*":
+        print multiply(args[0], args[1])
+    elif tokens[0] == "/":
+        print divide(args[0], args[1])
+    elif tokens[0] == "square":
+        print square(args[0])
+    elif tokens[0] == "cube":
+        print cube(args[0])
+    elif tokens[0] == "pow":
+        print power(args[0], args[1])
+    elif tokens[0] == "mod":
+        print mod(args[0], args[1])
+    else:
+        print "Bad input"
+
+    
